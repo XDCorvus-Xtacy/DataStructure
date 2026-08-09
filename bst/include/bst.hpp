aef1114137a24844bfcd57cbb5039ec8621b9184
XDcorvus-Xtacy <doxa0430@gmail.com>
2026-08-09 14:38:44 +0900

    feat: Add TreeNode struct and BST class declaration

--- bst/include/bst.hpp
+++ bst/include/bst.hpp
@@ -1,1 +1,26 @@
-// bst.hpp
+#pragma once
+
+struct TreeNode {
+    int key;             // 값 하나
+    TreeNode* left;      // 왼쪽 자식 (나보다 작은 값들)
+    TreeNode* right;     // 오른쪽 자식 (나보다 큰 값들)
+
+    TreeNode(int k)
+    {
+        key = k;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class BST {
+private:
+    TreeNode* root;
+
+public:
+    BST();
+    ~BST();
+    void insert(int key);
+    bool search(int key);
+    void remove(int key);
+};
