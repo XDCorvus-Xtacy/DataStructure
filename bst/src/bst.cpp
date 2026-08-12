f1d7ec9a767cde3686e42001ccd9f7fddfd3f41f
XDcorvus-Xtacy <doxa0430@gmail.com>
2026-08-12 21:10:00 +0900

    feat: Implement BST insert with recursion

--- bst/src/bst.cpp
+++ bst/src/bst.cpp
@@ -1,1 +1,19 @@
 // bst.cpp
+void BST::insert(int key)
+{
+    root = insertHelper(root, key);
+}
+
+//////////////////////////////////////////////////////////////
+TreeNode* BST::insertHelper(TreeNode* node, int key)
+{
+    if (node == nullptr)
+        return new TreeNode(key);
+
+    if (key < node->key)
+        node->left = insertHelper(node->left, key);
+    else if (key > node->key)
+        node->right = insertHelper(node->right, key);
+
+    return node;
+}
