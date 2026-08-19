cbc8d8fbe055fccfc6cea55bcac56bb14890757d
XDcorvus-Xtacy <doxa0430@gmail.com>
2026-08-19 18:46:51 +0900

    feat: Add Graph class declaration with adjacency list

---
+++ graph/include/graph.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+
+class Graph
+{
+private:
+    int vertexCount = 0;
+    std::vector<std::vector<int>> adj;
+
+public:
+    Graph(int n);
+    ~Graph();
+    void addEdge(int u, int v);
+};
