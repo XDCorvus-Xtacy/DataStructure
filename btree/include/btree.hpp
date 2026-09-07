e8ad92fb68cbceb339bbf5d63fa28d4c271e258b
XDcorvus-Xtacy <doxa0430@gmail.com>
2026-09-07 17:22:16 +0900

    feat: Add BTreeNode and BTree class declaration

--- btree/include/btree.hpp
+++ btree/include/btree.hpp
@@ -2,4 +2,26 @@
 #pragma once
 #include <vector>
 
-// TODO: BTreeNode, BTree 클래스 선언
+///////////////////////////////////////////////////////////
+struct BTreeNode {
+    std::vector<int> keys;
+    std::vector<BTreeNode*> children;
+    bool isLeaf;
+
+    BTreeNode(bool leaf) { isLeaf = leaf; }
+};
+
+///////////////////////////////////////////////////////////
+class BTree {
+private:
+    BTreeNode* root;
+    int order;
+
+public:
+    BTree(int order);      // 차수를 받아서 생성
+    ~BTree();
+    bool search(int key);  // 오늘/다음에 구현할 것
+    void insert(int key);  // 나중에
+};
+
+///////////////////////////////////////////////////////////
